df672e90f3a68f4844d6e49581f713cefbbb6a3c
Sophia Fraticelli <32481746+lahonu@users.noreply.github.com>
2019-10-28 16:34:08 +0000

    Update install.bat
    Changed from admin to non-admin designer python.exe

--- install.bat
+++ install.bat
@@ -1,1 +1,1 @@
-cd .. & "C:\Program Files\Alteryx\bin\Miniconda3\python.exe" -m venv .\venv & venv\Scripts\activate.bat & cd sfdc* & pip install -r requirements.txt
+cd .. & "%localappdata%\Alteryx\bin\Miniconda3\python.exe" -m venv .\venv & venv\Scripts\activate.bat & cd sfdc* & pip install -r requirements.txt
